787097f6e59a44599b95cf4be209cceb7de6f0d9
Abdullah Baig <abdullah.baig>
2024-03-14 22:41:08 +0500

    added try catch

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from dotenv import load_dotenv
 import sys
 
 # TODO:
-# 1. Add error handling and logging
-# 2. Logs are being duplicated in the log files. Need to fix this.
+# 1. Email Notification if script stops running or throws an error
 
 print("Python %s on %s" % (sys.version, sys.platform))
 
@@ -66,54 +65,66 @@ def update_last_processed_timestamp(timestamp):
 
 
 def fetch_cloudflare_logs(start_time: datetime, end_time: datetime):
-    headers = {
-        "X-Auth-Email": EMAIL,
-        "X-Auth-Key": API_KEY,
-        "Content-Type": "application/json",
-    }
-    # headers = {
-    #     "Authentication": BEARER,
-    #     "Content-Type": "application/json",
-    # }
-    url = f"https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/logs/received"
-    params = {
-        "start": start_time.isoformat(),
-        "end": end_time.isoformat(),
-        "fields": "ClientIP,ClientRequestHost,ClientRequestMethod,ClientRequestURI,EdgeEndTimestamp,EdgeResponseBytes,"
-                  "EdgeResponseStatus,EdgeStartTimestamp,RayID",
-    }
-    response = requests.get(url, headers=headers, params=params)
-    return [
-        json.loads(line) for line in response.iter_lines(decode_unicode=True) if line
-    ]
+    try:
+        headers = {
+            "X-Auth-Email": EMAIL,
+            "X-Auth-Key": API_KEY,
+            "Content-Type": "application/json",
+        }
+        # headers = {
+        #     "Authentication": BEARER,
+        #     "Content-Type": "application/json",
+        # }
+        url = f"https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/logs/received"
+        params = {
+            "start": start_time.isoformat(),
+            "end": end_time.isoformat(),
+            "fields": "ClientIP,ClientRequestHost,ClientRequestMethod,ClientRequestURI,EdgeEndTimestamp,EdgeResponseBytes,"
+                      "EdgeResponseStatus,EdgeStartTimestamp,RayID",
+        }
+        response = requests.get(url, headers=headers, params=params)
+        return [
+            json.loads(line) for line in response.iter_lines(decode_unicode=True) if line
+        ]
+    except Exception as e:
+        file_logger.error("An error occurred while fetching logs", exc_info=True)
+        syslog_logger.error("An error occurred while fetching logs", exc_info=True)
+        # email notification to be added
+        raise e
 
 
 def save_and_transmit_logs(logs, end_time):
-    latest_timestamp = None  # Initialize variable to track the latest timestamp
+    try:
+        latest_timestamp = None  # Initialize variable to track the latest timestamp
 
-    for record in logs:
-        # Convert EdgeStartTimestamp to datetime object
-        timestamp = datetime.fromtimestamp(record["EdgeStartTimestamp"] / 1_000_000_000.0, tz=timezone.utc)
+        for record in logs:
+            # Convert EdgeStartTimestamp to datetime object
+            timestamp = datetime.fromtimestamp(record["EdgeStartTimestamp"] / 1_000_000_000.0, tz=timezone.utc)
 
-        # Update latest_timestamp if this log's timestamp is newer
-        if latest_timestamp is None or timestamp > latest_timestamp:
-            latest_timestamp = timestamp
+            # Update latest_timestamp if this log's timestamp is newer
+            if latest_timestamp is None or timestamp > latest_timestamp:
+                latest_timestamp = timestamp
 
-        # Directory structure and file handling remains the same
-        directory = f"./log/cloudflare/{timestamp.strftime('%Y')}/{timestamp.strftime('%B')}/{timestamp.strftime('%d')}"
-        os.makedirs(directory, exist_ok=True)
-        filepath = os.path.join(directory, f"{timestamp.strftime('%H')}:00.cef")
-        cef_record = convert_to_cef(record)
+            # Directory structure and file handling remains the same
+            directory = f"./log/cloudflare/{timestamp.strftime('%Y')}/{timestamp.strftime('%B')}/{timestamp.strftime('%d')}"
+            os.makedirs(directory, exist_ok=True)
+            filepath = os.path.join(directory, f"{timestamp.strftime('%H')}:00.cef")
+            cef_record = convert_to_cef(record)
 
-        # Log to syslog server and file
-        syslog_handler.handle(logging.LogRecord("syslog_logger", logging.INFO, filepath, 0, cef_record,
-                                                [], None))
-        with open(filepath, "a") as file:
-            file.write(cef_record + "\n")
+            # Log to syslog server and file
+            syslog_handler.handle(logging.LogRecord("syslog_logger", logging.INFO, filepath, 0, cef_record,
+                                                    [], None))
+            with open(filepath, "a") as file:
+                file.write(cef_record + "\n")
 
-    # Update the last_processed_timestamp to the end_time of this execution
-    if logs:  # Update only if there are new logs processed
-        update_last_processed_timestamp(end_time)
+        # Update the last_processed_timestamp to the end_time of this execution
+        if logs:  # Update only if there are new logs processed
+            update_last_processed_timestamp(end_time)
+    except Exception as e:
+        file_logger.error("An error occurred while saving logs", exc_info=True)
+        syslog_logger.error("An error occurred while saving logs", exc_info=True)
+        # email notification to be added
+        raise e
 
 
 def convert_to_cef(record: dict):
@@ -138,29 +149,36 @@ def convert_to_cef(record: dict):
 
 
 def main():
-    current_time = datetime.now(tz.tzutc())
-    last_processed_time = get_last_processed_timestamp()
+    try:
+        current_time = datetime.now(tz.tzutc())
+        last_processed_time = get_last_processed_timestamp()
 
-    # Loop through each hour from the last_processed_time up to the current_time
-    while last_processed_time < current_time:
-        # Calculate the start and end time for the current hour block
-        start_time = last_processed_time
-        end_time = min(start_time + timedelta(hours=1), current_time)
+        # Loop through each hour from the last_processed_time up to the current_time
+        while last_processed_time < current_time:
+            # Calculate the start and end time for the current hour block
+            start_time = last_processed_time
+            end_time = min(start_time + timedelta(hours=1), current_time)
 
-        # Update last_processed_time for the next iteration
-        last_processed_time = end_time
+            # Update last_processed_time for the next iteration
+            last_processed_time = end_time
 
-        # Fetch logs for the current hour block
-        logs = fetch_cloudflare_logs(start_time, end_time)
+            # Fetch logs for the current hour block
+            logs = fetch_cloudflare_logs(start_time, end_time)
 
-        # Process and save the logs
-        if logs:
-            save_and_transmit_logs(logs, end_time)
-            # Update the last_processed_timestamp to the end_time of this hour block
-            update_last_processed_timestamp(end_time)
-            print(f"Logs between {start_time} and {end_time} have been processed.")
-        else:
-            print(f"No new logs to process between {start_time} and {end_time}.")
+            # Process and save the logs
+            if logs:
+                save_and_transmit_logs(logs, end_time)
+                # Update the last_processed_timestamp to the end_time of this hour block
+                update_last_processed_timestamp(end_time)
+                print(f"Logs between {start_time} and {end_time} have been processed.")
+            else:
+                print(f"No new logs to process between {start_time} and {end_time}.")
+
+    except Exception as e:
+        file_logger.error("An error occurred", exc_info=True)
+        syslog_logger.error("An error occurred", exc_info=True)
+        # email notification to be added
+        raise e
 
 
 if __name__ == "__main__":
